85e80398fed4c5ddbd6d5939a69014ce736a8692
Max <sydneymax@gmail.com>
2018-08-18 23:11:18 +1000

    Outputting shopping list

--- src/app/components/shopping-bucket/shopping-bucket.component.ts
+++ src/app/components/shopping-bucket/shopping-bucket.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-bucket',
   templateUrl: './shopping-bucket.component.html'
 })
 export class ShoppingBucketComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 10)
+  ];
 
   constructor() { }
 
