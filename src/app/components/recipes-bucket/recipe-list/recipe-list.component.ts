d18992e57f6f440d9ed3a6de040751db38cd2a50
Max <sydneymax@gmail.com>
2018-08-18 22:32:47 +1000

    Outputting a list of recipes

--- src/app/components/recipes-bucket/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes-bucket/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe #1',
+      'This is simply a test #1',
+      'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg'),
+    new Recipe(
+      'A Test Recipe #2',
+      'This is simply a test #2',
+      'https://img.delicious.com.au/xWjPCEi5/h506-w759-cfill/del/2015/10/del1015p107-1-17294-1.jpg')
+  ]; 
 
   constructor() { }
 
