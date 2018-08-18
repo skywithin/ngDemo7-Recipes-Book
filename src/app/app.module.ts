cf744f59114afbabb032b6392f7237643879e3aa
Max <sydneymax@gmail.com>
2018-08-18 21:14:32 +1000

    Create all RecipeBucket related components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponet } from './components/header/header.component';
+import { RecipesBucketComponent } from './components/recipes-bucket/recipes-bucket.component';
+import { RecipeListComponent } from './components/recipes-bucket/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipes-bucket/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipes-bucket/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponet
+    HeaderComponet,
+    RecipesBucketComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/recipes-bucket/recipes-bucket.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-bucket',
+  templateUrl: './recipes-bucket.component.html'
+})
+export class RecipesBucketComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
