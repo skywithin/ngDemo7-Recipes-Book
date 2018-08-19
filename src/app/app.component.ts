9eec19bb6342c4d5bdb3b60ba3daa59a95b6acf2
Max <sydneymax@gmail.com>
2018-08-19 22:56:24 +1000

    Adding navigation with event binding ad ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngDemo7-Recipes-Book';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
