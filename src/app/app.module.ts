f5ae8ab62efaf305098122565b8b88eceb6ad91c
Max <sydneymax@gmail.com>
2018-09-02 20:41:14 +1000

    Allow to add ingredients to the shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponet } from './components/header/header.component';
@@ -22,7 +23,8 @@ import { ShoppingEditComponent } from './components/shopping-Bucket/shopping-edi
     ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
