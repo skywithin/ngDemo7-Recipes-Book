d101f014a21f39e7c903a825d6cc37577ae4fe97
Max <sydneymax@gmail.com>
2018-10-01 17:57:22 +1000

    222 - Redirecting the user after deleting a recipe

--- src/app/components/recipes-bucket/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes-bucket/recipe-detail/recipe-detail.component.ts
@@ -37,5 +37,6 @@ export class RecipeDetailComponent implements OnInit {
 
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.selectedRecipeId);
+    this.router.navigate(['/recipes']);
   }
 }
