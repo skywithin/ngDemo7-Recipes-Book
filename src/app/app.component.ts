0774f372355cceabe9234038cc4f1374487ddbe0
Max <sydneymax@gmail.com>
2018-10-05 21:23:34 +1000

    259 - Setting up the Firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ngDemo7-Recipes-Book';
   loadedFeature: string = 'recipe';
+  
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCsxR7wpi-zQgoLtulmr4e0lwOYkJrmwJc",
+      authDomain: "mvagner-udemy-ng-recipe-book.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
