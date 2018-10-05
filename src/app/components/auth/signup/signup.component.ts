b3f585ea1df1a3cb2f3a35c9e8c7522f820b38e1
Max <sydneymax@gmail.com>
2018-10-05 21:40:54 +1000

    261 - Signing users up

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,13 +8,15 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onSignup(form: NgForm) {
+    //console.log('onSignup() triggered')
     const email = form.value.email;
     const password = form.value.password;
+    this.authService.signupUser(email, password);
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,16 @@ export class AuthService {
   constructor() { }
 
   signupUser(email: string, password: string) {
-    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(
+        () => {
+          console.log('User created');
+        }
+      )
+      .catch(
+        (error) => {
+          console.log(error);
+        }
+      )
   }
 }
