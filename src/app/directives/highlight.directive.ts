f36160a381fc280c1f2403edec2e52b06a71a52b
Max <sydneymax@gmail.com>
2018-10-14 14:11:46 +1100

    Remove unused directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,42 +1,42 @@
-import { 
-    Directive, 
-    OnInit, 
-    Renderer2, 
-    ElementRef, 
-    HostListener, 
-    HostBinding, 
-    Input} from "@angular/core";
+// import { 
+//     Directive, 
+//     OnInit, 
+//     Renderer2, 
+//     ElementRef, 
+//     HostListener, 
+//     HostBinding, 
+//     Input} from "@angular/core";
 
-@Directive({
-    selector: '[appHighlight]'
-})
-export class HighlightDirective implements OnInit {
+// @Directive({
+//     selector: '[appHighlight]'
+// })
+// export class HighlightDirective implements OnInit {
 
-    @Input() defaultColor: string = 'transparent';
-    @Input('appHighlight') highlightColor: string = 'blue';
+//     @Input() defaultColor: string = 'transparent';
+//     @Input('appHighlight') highlightColor: string = 'blue';
 
-    @HostBinding('style.backgroundColor') backgroundColor: string;
-    @HostBinding('style.color') textColor: string = 'black';
+//     @HostBinding('style.backgroundColor') backgroundColor: string;
+//     @HostBinding('style.color') textColor: string = 'black';
 
-    constructor(
-        private elementRef: ElementRef,
-        private renderer: Renderer2) { }
+//     constructor(
+//         private elementRef: ElementRef,
+//         private renderer: Renderer2) { }
 
-    ngOnInit(){
-        this.backgroundColor = this.defaultColor;
-    }
+//     ngOnInit(){
+//         this.backgroundColor = this.defaultColor;
+//     }
 
-    @HostListener('mouseenter') mouseover(eventData: Event) {
-        //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-        this.backgroundColor = this.highlightColor;
-        //this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
-        this.textColor = 'white';
-    }
+//     @HostListener('mouseenter') mouseover(eventData: Event) {
+//         //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+//         this.backgroundColor = this.highlightColor;
+//         //this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+//         this.textColor = 'white';
+//     }
     
-    @HostListener('mouseleave') mouselease(eventData: Event) {
-        //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-        this.backgroundColor = this.defaultColor;
-        //this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
-        this.textColor = 'black';
-    }
-}
+//     @HostListener('mouseleave') mouselease(eventData: Event) {
+//         //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+//         this.backgroundColor = this.defaultColor;
+//         //this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+//         this.textColor = 'black';
+//     }
+// }
